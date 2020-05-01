b257cc62b1231edfd8bf7a59760954d15f340ee1
Rick Hightower <rick.hightower@aboutobjects.com>
2020-04-30 21:10:29 -0700

    fixed version pull

--- build/deploy-bridge-admin-docker.sh
+++ build/deploy-bridge-admin-docker.sh
@@ -1,5 +1,10 @@
-# Build admin image 
+# Build admin image
 docker build -t cloudurable/nats-bridge-admin -f cicd/Dockerfile_bridge.dev .
 
 # Publish admin image
 docker push cloudurable/nats-bridge-admin
+
+
+docker tag cloudurable/nats-bridge-admin cloudurable/nats-bridge-admin:0.0.1
+
+docker push cloudurable/nats-bridge-admin:0.0.1
