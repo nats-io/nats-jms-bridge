c0640bcaa93d9fa91720d097f38700cfce9d4a90
Rick Hightower <rick.hightower@aboutobjects.com>
2020-05-14 21:37:19 -0700

    added error handler

--- admin/src/main/kotlin/io/nats/bridge/admin/runner/support/HealthChecker.kt
+++ admin/src/main/kotlin/io/nats/bridge/admin/runner/support/HealthChecker.kt
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component
 @Component
 class HealthChecker  (private val messageBridgeRunner:MessageBridgeRunner): HealthIndicator {
     override fun health(): Health {
-
         return if (!messageBridgeRunner.isHealthy()) {
             Health.down().withDetail("NATS_MessageBridge", "Not Available - Was Started? ${messageBridgeRunner.wasStarted()} Is Running? ${messageBridgeRunner.isRunning()} ${messageBridgeRunner.getLastError()?.message} ${messageBridgeRunner.getLastError()?.javaClass?.simpleName}" ).build()
         } else Health.up().withDetail("NATS_MessageBridge", "Available").build()
