b98411c5f036c9765cc3740d8cc813842517515e
Rick Hightower <rick.hightower@aboutobjects.com>
2020-06-07 15:13:18 -0700

    add example and add fix

--- examples/src/main/java/io/nats/bridge/examples/jms/SimpleJmsServer.java
+++ examples/src/main/java/io/nats/bridge/examples/jms/SimpleJmsServer.java
@@ -3,10 +3,11 @@ package io.nats.bridge.examples.jms;
 import io.nats.bridge.examples.JmsBuildUtils;
 
 import javax.jms.*;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 
 public class SimpleJmsServer {
-    public static void main(final String[] args)  {
+    public static void main(final String[] args) {
 
         final Duration waitForMessage = Duration.ofSeconds(20);
 
@@ -17,30 +18,52 @@ public class SimpleJmsServer {
 
             final MessageConsumer messageConsumer = jmsBuildUtils.getConsumerSupplier().get();
 
+            int count = 0;
 
-            System.out.println("About to get Message");
-            final Message messageFromClient = messageConsumer.receive(waitForMessage.toMillis());
-            System.out.println("Attempted to get Message");
+            while (count < 100) {
+                System.out.println("About to get Message");
+                final Message messageFromClient = messageConsumer.receive(waitForMessage.toMillis());
+                System.out.println("Attempted to get Message");
 
-            if (messageFromClient == null) {
-                System.out.println("No message found");
-            } else {
+                if (messageFromClient == null) {
+                    System.out.println("No message found");
+                    count ++;
+                } else {
 
-                System.out.println("Got the message now respond");
-                final MessageProducer producer = session.createProducer(messageFromClient.getJMSDestination());
+                    System.out.println("Got the message now respond " + messageFromClient.getJMSReplyTo()
+                            + " " + messageFromClient.getJMSCorrelationID());
+                    final MessageProducer producer = session.createProducer(messageFromClient.getJMSReplyTo());
 
-                if (messageFromClient instanceof TextMessage) {
-                    final TextMessage requestMessage = (TextMessage) messageFromClient;
+                    System.out.println("Got the producer now respond ");
 
-                    final String responseText = "Server Got: " + requestMessage.getText() + " thank you";
-                    final TextMessage replyMessage = session.createTextMessage(responseText);
-                    producer.send(replyMessage);
+                    if (messageFromClient instanceof BytesMessage) {
+                        final BytesMessage requestMessage = (BytesMessage) messageFromClient;
+
+                        final int length =  (int) requestMessage.getBodyLength();
+
+                        final byte buffer[] = new byte[length];
+
+                        requestMessage.readBytes(buffer);
+
+
+                        final String message = new String(buffer, StandardCharsets.UTF_8);
+                        final String responseText = "Server Got: " + message + " thank you";
+                        final BytesMessage replyMessage = session.createBytesMessage();
+
+                        replyMessage.setJMSCorrelationID(messageFromClient.getJMSCorrelationID());
+                        replyMessage.writeBytes(responseText.getBytes(StandardCharsets.UTF_8));
+                        producer.send(replyMessage);
+
+                        System.out.println("SENT: " + responseText);
+                    } else {
+                        System.out.println("Message was not a bytes message " + messageFromClient);
+                    }
+
                 }
-
             }
 
 
-        }catch (Exception ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
             System.exit(1);
         }
