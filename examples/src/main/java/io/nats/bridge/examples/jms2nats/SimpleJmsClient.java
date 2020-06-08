61ebdffade7bfe5e8e188a1d1d731257ec6b47eb
Rick Hightower <rick.hightower@aboutobjects.com>
2020-06-07 17:26:44 -0700

    added a second example from JMS to NATS

---
+++ examples/src/main/java/io/nats/bridge/examples/jms2nats/SimpleJmsClient.java
@@ -0,0 +1,48 @@
+package io.nats.bridge.examples.jms2nats;
+
+import io.nats.bridge.examples.JmsBuildUtils;
+
+import javax.jms.*;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.UUID;
+
+public class SimpleJmsClient {
+
+    public static void main (String[] args) {
+        try {
+
+            final JmsBuildUtils jmsBuildUtils = new JmsBuildUtils().withDestinationName("dynamicQueues/client-requests");
+            final Session session = jmsBuildUtils.getSession();
+            final MessageProducer messageProducer = jmsBuildUtils.getProducerSupplier().get();
+            final Destination replyQueue = session.createTemporaryQueue();
+            final Duration replyTimeoutDuration = Duration.ofSeconds(30);
+
+            final BytesMessage requestMessage = session.createBytesMessage();
+            requestMessage.writeBytes("Hello World!".getBytes(StandardCharsets.UTF_8));
+            requestMessage.setJMSCorrelationID(UUID.randomUUID().toString());
+            requestMessage.setJMSReplyTo(replyQueue);
+
+            messageProducer.send(requestMessage);
+
+            final MessageConsumer replyConsumer = session.createConsumer(replyQueue);
+
+            final Message reply = replyConsumer.receive(replyTimeoutDuration.toMillis());
+
+            if (reply instanceof BytesMessage) {
+                final BytesMessage bytesReply = (BytesMessage) reply;
+                final byte[] buffer = new  byte[(int)bytesReply.getBodyLength()];
+                bytesReply.readBytes(buffer);
+
+                System.out.println("REPLY: " + new String(buffer, StandardCharsets.UTF_8));
+            } else {
+                System.out.println("No reply message came back or wrong type");
+            }
+
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
