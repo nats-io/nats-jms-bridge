da52e35b22defd82645215aee7a8b7b4723fe794
Rick Hightower <rick.hightower@aboutobjects.com>
2020-06-15 12:40:30 -0700

    inital stab at base 64 encoded values for streams and fixing duplicate use of the same password for the trust store and key store

--- core/src/main/java/io/nats/bridge/tls/SslContextBuilder.java
+++ core/src/main/java/io/nats/bridge/tls/SslContextBuilder.java
@@ -1,18 +1,102 @@
 package io.nats.bridge.tls;
 
 
-import javax.net.ssl.SSLContext;
-import java.io.File;
+import io.nats.client.Options;
+
+import javax.net.ssl.*;
+import java.io.*;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+import java.util.Base64;
 
 public class SslContextBuilder {
 
     private String truststorePath;
     private String keystorePath;
     private String algorithm;
-    private String keyPassword;
-    private  String storePassword;
+    private char[] keystorePassword;
+    private char[] trustStorePassword;
+    private TrustManager[] trustStoreKeyManagers;
+    private KeyManager[] keyStoreKeyManagers;
+    private String trustStoreValueEnvVariable = "TRUSTSTORE";
+    private String keyStoreValueEnvVariable = "KEYSTORE";
+    private String trustStorePathEnvVariable = "TRUSTSTORE_PATH";
+    private String keyStorePathEnvVariable = "KEYSTORE_PATH";
+
+
+    public String getTrustStoreValueEnvVariable() {
+        return trustStoreValueEnvVariable;
+    }
+
+    public SslContextBuilder setTrustStoreValueEnvVariable(String trustStoreValueEnvVariable) {
+        this.trustStoreValueEnvVariable = trustStoreValueEnvVariable;
+        return this;
+    }
+
+    public String getKeyStoreValueEnvVariable() {
+        return keyStoreValueEnvVariable;
+    }
+
+    public SslContextBuilder setKeyStoreValueEnvVariable(String keyStoreValueEnvVariable) {
+        this.keyStoreValueEnvVariable = keyStoreValueEnvVariable;
+        return this;
+    }
+
+    public String getTrustStorePathEnvVariable() {
+        return trustStorePathEnvVariable;
+    }
+
+    public SslContextBuilder setTrustStorePathEnvVariable(String trustStorePathEnvVariable) {
+        this.trustStorePathEnvVariable = trustStorePathEnvVariable;
+        return this;
+    }
+
+    public String getKeyStorePathEnvVariable() {
+        return keyStorePathEnvVariable;
+    }
+
+    public SslContextBuilder setKeyStorePathEnvVariable(String keyStorePathEnvVariable) {
+        this.keyStorePathEnvVariable = keyStorePathEnvVariable;
+        return this;
+    }
+
+    public SslContextBuilder setTruststorePath(String truststorePath) {
+        this.truststorePath = truststorePath;
+        return this;
+    }
+
+    public SslContextBuilder setKeystorePath(String keystorePath) {
+        this.keystorePath = keystorePath;
+        return this;
+    }
+
+
+    private static KeyStore loadKeystore(final InputStream in, final char[] password) throws Exception {
+        final KeyStore store = KeyStore.getInstance("JKS");
+
+        try {
+            store.load(in, password);
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+
+        return store;
+
+    }
+
+    public SslContextBuilder withTrustStoreKeyManagers(TrustManager[] trustStoreKeyManagers) {
+        this.trustStoreKeyManagers = trustStoreKeyManagers;
+        return this;
+    }
 
     public String getTruststorePath() {
+
+        if (truststorePath == null) {
+            truststorePath = System.getenv(this.getTrustStorePathEnvVariable());
+        }
+        if (truststorePath!=null)
         validatePath("TruststorePath", truststorePath);
         return truststorePath;
     }
@@ -41,6 +125,12 @@ public class SslContextBuilder {
     }
 
     public String getKeystorePath() {
+
+        if (keystorePath == null) {
+            keystorePath = System.getenv(getKeyStorePathEnvVariable());
+        }
+
+        if (keystorePath!=null)
         validatePath("keystorePath", keystorePath);
         return keystorePath;
     }
@@ -62,29 +152,116 @@ public class SslContextBuilder {
         return this;
     }
 
-    public String getKeyPassword() {
-        return keyPassword;
+    public char[] getKeystorePassword() {
+        return keystorePassword;
     }
 
     public SslContextBuilder withKeyPassword(final String keyPassword) {
-        this.keyPassword = keyPassword;
+        this.keystorePassword = keyPassword.toCharArray();
         return this;
     }
 
-    public String getStorePassword() {
-        return storePassword;
+    public char[] getTrustStorePassword() {
+        return trustStorePassword;
     }
 
     public SslContextBuilder withStorePassword(final String storePassword) {
-        this.storePassword = storePassword;
+        this.trustStorePassword = storePassword.toCharArray();
         return this;
+    }
+
+    public SSLContext buildOld() {
+        try {
+            return SSLUtils.createSSLContext(getTruststorePath(), getKeystorePath(), getAlgorithm(), new String(getKeystorePassword()),
+                    new String(getTrustStorePassword()));
+        } catch (Exception e) {
+            throw new SslContextBuilderException("Unable to create SSL context", e);
+        }
     }
 
     public SSLContext build() {
         try {
-            return SSLUtils.createSSLContext(getTruststorePath(), getKeystorePath(), getAlgorithm(), getKeyPassword(), getStorePassword());
+            SSLContext ctx = SSLContext.getInstance(Options.DEFAULT_SSL_PROTOCOL);
+            ctx.init(getKeyStoreKeyManagers(), getTrustStoreKeyManagers(), new SecureRandom());
+            return ctx;
         } catch (Exception e) {
             throw new SslContextBuilderException("Unable to create SSL context", e);
         }
+    }
+
+
+    public TrustManager[] getTrustStoreKeyManagers() {
+        if (trustStoreKeyManagers == null) {
+            final String truststorePath = getTruststorePath();
+            if (truststorePath == null) {
+                final String value = System.getenv(getTrustStoreValueEnvVariable());
+                byte[] decode = Base64.getDecoder().decode(value);
+                try {
+                    final KeyStore store =  loadKeystore(new ByteArrayInputStream(decode),
+                            getTrustStorePassword());
+
+
+                    final TrustManagerFactory factory = TrustManagerFactory.getInstance(getAlgorithm());
+                    factory.init(store);
+                    trustStoreKeyManagers = factory.getTrustManagers();
+                } catch (Exception e) {
+                    throw new IllegalStateException("Unable to load Trust store from env variable" + getTrustStoreValueEnvVariable(), e);
+                }
+            } else {
+                try {
+                    final FileInputStream fileInputStream = new FileInputStream(truststorePath);
+                    final KeyStore store =  loadKeystore(fileInputStream, getTrustStorePassword());
+                    final TrustManagerFactory factory = TrustManagerFactory.getInstance(getAlgorithm());
+                    factory.init(store);
+                    trustStoreKeyManagers = factory.getTrustManagers();
+                } catch (FileNotFoundException e) {
+                    throw new IllegalStateException("Trust store path not found" + truststorePath, e);
+                } catch (Exception e) {
+                    throw new IllegalStateException("Unable to load Trust store with path" + truststorePath, e);
+                }
+            }
+        }
+        return trustStoreKeyManagers;
+    }
+
+
+    public KeyManager[] getKeyStoreKeyManagers() {
+
+        if (keyStoreKeyManagers == null) {
+            final String keyStorePath = getKeystorePath();
+            if (keyStorePath == null) {
+                final String value = System.getenv(getKeyStoreValueEnvVariable());
+
+                byte[] decode = Base64.getDecoder().decode(value);
+                try {
+                   final KeyStore store =  loadKeystore(new ByteArrayInputStream(decode),
+                             getKeystorePassword());
+                    final KeyManagerFactory factory = KeyManagerFactory.getInstance(getAlgorithm());
+                    factory.init(store, getKeystorePassword());
+                    keyStoreKeyManagers = factory.getKeyManagers();
+                } catch (Exception e) {
+                    throw new IllegalStateException("Unable to load key store from env variable" + getKeyStoreValueEnvVariable(), e);
+                }
+            } else {
+                try {
+                    final FileInputStream fileInputStream = new FileInputStream(keyStorePath);
+                    final KeyStore store  = loadKeystore(fileInputStream, getKeystorePassword());
+                    final KeyManagerFactory factory = KeyManagerFactory.getInstance(getAlgorithm());
+                    factory.init(store, getKeystorePassword());
+                    keyStoreKeyManagers = factory.getKeyManagers();
+                } catch (FileNotFoundException e) {
+                    throw new IllegalStateException("Key store path not found" + keyStorePath, e);
+                } catch (Exception e) {
+                    throw new IllegalStateException("Unable to load Key store with path" + keyStorePath, e);
+                }
+            }
+        }
+        return keyStoreKeyManagers;
+
+    }
+
+    public SslContextBuilder withKeyStoreKeyManagers(KeyManager[] keyStoreKeyManagers) {
+        this.keyStoreKeyManagers = keyStoreKeyManagers;
+        return this;
     }
 }
