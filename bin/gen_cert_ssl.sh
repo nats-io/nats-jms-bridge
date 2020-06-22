9a116a81f3752916d3e93256c721f04dd5865efd
Rick Hightower <rick.hightower@aboutobjects.com>
2020-06-22 15:03:49 -0700

    new ssl files for beta 7

--- bin/gen_cert_ssl.sh
+++ bin/gen_cert_ssl.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 set -e -i
 
-PATH=${PATH:/certs}
-P12=${P12:keystore.p12}
-P12PASS=${P12PASS:cloudurable}
-CERTFILE=${CERTFILE:localhost.pem}
-CERTFILEKEY=${CERTFILEKEY:localhost-key.pem}
-KNAME=${KNAME:keystore.jks}
-KPASS=${KPASS:cloudurable}
-ROOTCA=${ROOTCA:rootCA.pem}
-RPASS=${RPASS:cloudurable}
-TNAME=${TNAME:truststore.jks}
+CERT_PATH=${CERT_PATH:-./certs}
+P12=${P12:-keystore.p12}
+P12PASS=${P12PASS:-cloudurable0}
+CERTFILE=${CERTFILE:-localhost.pem}
+CERTFILEKEY=${CERTFILEKEY:-localhost-key.pem}
+KNAME=${KNAME:-keystore.jks}
+KEY_PASSCODE=${KEY_PASSCODE:-cloudurable1}
+ROOTCA=${ROOTCA:-localhost.pem}
+TRUST_PASSCODE=${TRUST_PASSCODE:-cloudurable2}
+TNAME=${TNAME:-truststore.jks}
 
 # Java SSLUtils requires certificates to be in the java keystore format (.jks).
 # To do that you need JDK installed and use openssl to generate a pkcs12 file (.p12) using the certificate file (localhost.pem) and the certificate key (localhost-key.pem).
@@ -18,13 +18,13 @@ TNAME=${TNAME:truststore.jks}
 # keytool is also used to import the CA certificate rootCA.pem into truststore.jks.
 
 echo "Generating keystore.p12 file......"
-openssl pkcs12 -export -out $PATH/$P12 -inkey $PATH/$CERTFILEKEY -in $PATH/$CERTFILE -password pass:$P12PASS
+openssl pkcs12 -export -out $CERT_PATH/$P12 -inkey $CERT_PATH/$CERTFILEKEY -in $CERT_PATH/$CERTFILE -password pass:$P12PASS
 echo "File generated"
 echo ".............."
 echo "Generating keystore.jks file......"
-keytool -importkeystore -srcstoretype PKCS12 -srckeystore $PATH/$P12 -srcstorepass $P12PASS -destkeystore $PATH/$KNAME -deststorepass $KPASS
+keytool -importkeystore -srcstoretype PKCS12 -srckeystore $CERT_PATH/$P12 -srcstorepass $P12PASS -destkeystore $CERT_PATH/$KNAME -deststorepass $KEY_PASSCODE
 echo "File generated"
 echo ".............."
 echo "Generating truststore.jks file......"
-keytool -importcert -trustcacerts -file $PATH/$ROOTCA -storepass $RPASS -noprompt -keystore $PATH/$TNAME
+keytool -importcert -trustcacerts -file $CERT_PATH/$ROOTCA -storepass $TRUST_PASSCODE -noprompt -keystore $CERT_PATH/$TNAME
 echo "File generated"
