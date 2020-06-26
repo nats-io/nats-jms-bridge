862af8ce19a43b99dd5eb26ff1b44e1fd206a24c
Rick Hightower <rick.hightower@aboutobjects.com>
2020-06-26 12:06:16 -0700

    transform message #183

--- message/src/main/java/io/nats/bridge/messages/transform/TransformResult.java
+++ message/src/main/java/io/nats/bridge/messages/transform/TransformResult.java
@@ -1,3 +1,15 @@
+// Copyright 2020 The NATS Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package io.nats.bridge.messages.transform;
 
 import io.nats.bridge.messages.Message;
@@ -5,6 +17,9 @@ import io.nats.bridge.messages.Message;
 import java.util.Objects;
 import java.util.Optional;
 
+/**
+ * The results of a transformation which includes status message and if the message was transformed or not and may contain errors (exceptions).
+ */
 public class TransformResult {
 
     private final Optional<Throwable> error;
