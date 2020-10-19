92ed2fd0227418bfe0ad5293ca8318d5b3c77bff
Rick Hightower <rick.hightower@aboutobjects.com>
2020-10-19 01:00:31 -0700

    ccdt only runs locally

--- message/build.gradle.kts
+++ message/build.gradle.kts
@@ -47,7 +47,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "io.nats.bridge"
             artifactId = "nats-jms-bridge-message"
-            version = "0.27.2-beta24"
+            version = "0.27.3-beta24"
             from(components["java"])
         }
     }
