6bf66214d3d3c77c8ba63c5af8f235732d1a3157
scottf <scottfauerbach@gmail.com>
2021-06-10 18:55:47 -0400

    working on the admin

--- admin/build.gradle.kts
+++ admin/build.gradle.kts
@@ -13,6 +13,13 @@ buildscript {
     }
 }
 
+
+val jarVersion = "0.29.0-beta27"
+val isRelease = System.getenv("BUILD_EVENT") == "publish_release"
+
+// version is the variable the build actually uses.
+var pubVersion = if (isRelease) jarVersion else jarVersion + "-SNAPSHOT"
+
 plugins {
 
     val kotlinVersion = "1.3.71"
@@ -25,8 +32,8 @@ plugins {
     distribution
     id("org.springframework.boot") version "2.2.6.RELEASE"
     `maven-publish`
-    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
-    id("signing")
+
+
 }
 
 repositories {
@@ -37,19 +44,15 @@ repositories {
     mavenCentral()
 }
 
-val jarVersion = "0.29.0-beta27"
-val isRelease = System.getenv("BUILD_EVENT") == "publish_release"
 
-// version is the variable the build actually uses.
-version = if (isRelease) jarVersion else jarVersion + "-SNAPSHOT"
 
 springBoot {
     mainClassName = "io.nats.bridge.admin.ApplicationMain"
     buildInfo {
         properties {
             additional = mapOf(
-                    "release" to "Beta2",
-                    "author" to "NATS team"
+                "release" to "Beta2",
+                "author" to "NATS team"
             )
         }
     }
@@ -57,15 +60,6 @@ springBoot {
 
 application {
     mainClassName = "io.nats.bridge.admin.ApplicationMain"
-}
-
-nexusPublishing {
-    repositories {
-        sonatype {
-            username.set(System.getenv("OSSRH_USERNAME"))
-            password.set(System.getenv("OSSRH_PASSWORD"))
-        }
-    }
 }
 
 publishing {
@@ -77,31 +71,19 @@ publishing {
             groupId = "io.nats.bridge"
             artifactId = "nats-jms-bridge-springboot-app"
 
+            version = pubVersion
+
             from(components["java"])
-            pom {
-                name.set(rootProject.name)
-                packaging = "jar"
-                groupId = "io.nats.bridge"
-                artifactId = "nats-jms-bridge-springboot-app"
-                description.set("NATS.IO Java JMS Bridge Admin App")
-                url.set("https://github.com/nats-io/nats-jms-bridge")
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("synadia")
-                        name.set("Synadia")
-                        email.set("info@synadia.com")
-                        url.set("https://nats.io")
-                    }
-                }
-                scm {
-                    url.set("https://github.com/nats-io/nats-jms-bridge")
-                }
+        }
+    }
+    repositories {
+        maven {
+            name = "OSSRH"
+            //url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            url = uri("https://oss.sonatype.org/service/local/repositories/releases/content/")
+            credentials {
+                username = System.getenv("OSSRH_USERNAME")
+                password = System.getenv("OSSRH_PASSWORD")
             }
         }
     }
@@ -150,7 +132,10 @@ tasks.getByName<BootJar>("bootJar") {
     launchScript()
 
     archiveClassifier.set("boot")
+
 }
+
+
 
 tasks.getByName<Jar>("jar") {
     enabled = true
@@ -161,6 +146,7 @@ tasks.getByName<CreateStartScripts>("startScripts") {
     val gen = unixStartScriptGenerator as org.gradle.api.internal.plugins.UnixStartScriptGenerator
     gen.template = resources.text.fromFile(file("src/main/bash/unix.txt"))
 }
+
 
 tasks {
 
@@ -248,7 +234,7 @@ tasks {
         // Swagger
         implementation("io.springfox:springfox-swagger-ui:2.7.0")
         implementation("io.springfox:springfox-swagger2:2.7.0")
-        implementation("io.nats.bridge:nats-jms-bridge:0.29.0-beta27")
+        implementation("io.nats.bridge:nats-jms-bridge:0.29.0-beta26")
         implementation("io.micrometer:micrometer-registry-prometheus:1.3.6")
 
         // https://mvnrepository.com/artifact/com.ibm.mq/com.ibm.mq.allclient
@@ -264,7 +250,7 @@ tasks {
         implementation("io.jsonwebtoken:jjwt-api:0.11.1")
         implementation("io.jsonwebtoken:jjwt-impl:0.11.1")
         implementation("io.jsonwebtoken:jjwt-jackson:0.11.1")
-        implementation("io.nats:jnats:2.11.4")
+        implementation("io.nats:jnats:2.6.7")
 
 
         // https://mvnrepository.com/artifact/com.squareup.okhttp3/okhttp
@@ -273,9 +259,14 @@ tasks {
 
         implementation("com.github.ajalt:clikt:2.7.1")
 
-        implementation("io.nats.bridge:nats-jms-bridge-message:0.29.0-beta27")
+        implementation("io.nats.bridge:nats-jms-bridge-message:0.29.0-beta26")
 
-        implementation("io.nats.bridge:nats-jms-bridge:0.29.0-beta27")
+        implementation("io.nats.bridge:nats-jms-bridge:0.29.0-beta26")
+
+        //implementation("io.nats.bridge:nats-jms-bridge-example-transforms:0.29.0-beta26")
+
+
+
 
     }
 
